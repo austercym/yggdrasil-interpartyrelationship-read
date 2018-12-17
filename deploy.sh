7538c80899b344be2d90984e6900ad3d064ffe6b
Miguel Barrios Molina <m.barriosmolina@orwellg.com>
2018-12-17 15:43:19 +0100

    Improved deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,33 @@
 #!/usr/bin/env bash
 
-usage() { 
-	echo "Usage: $0 -v <version> [-d <dest-server>] [-f]" 1>&2; exit 1; 
+usage() {
+	echo "Usage: $0 -v <version> [-e <env>] [-d <dest-server>] [-u <server-user>] [-f]" 1>&2;
+	echo "  - env: SID / UAT; currently used ONLY for some config, so make sure you provide correct arguments to -d and -u options";
+	exit 1;
 }
 
-# Default dest-server is SID
+# Default env
+env=SID
+
+# Default dest-server for SID
 server=sid-hdf-g4-1
 
-while getopts "v:d:f" o; do
+# Default server-user for SID
+serverUser=centos
+
+while getopts "v:e:d:u:f" o; do
     case "${o}" in
         v)
             version=${OPTARG}
             ;;
+        e)
+            env=${OPTARG}
+            ;;
         d)
             server=${OPTARG}
+            ;;
+        u)
+            serverUser=${OPTARG}
             ;;
         f)
             force=true
@@ -32,7 +46,7 @@ fi
 if [ -z "${force}" ]; then
 	read -r -p "Will deploy to $server, are you sure? [y/N] " response
 	case "$response" in
-	    [yY][eE][sS]|[yY]) 
+	    [yY][eE][sS]|[yY])
 	        echo "deploying..."
 	        ;;
 	    *)
@@ -49,8 +63,17 @@ PROFILE=deploy
 SERVERUSER=centos
 SERVERDIR=/tmp/
 DEPLOYUSER=svc_core
-NIMBUS=sid-hdf-g1-1.node.sid.consul
-ZOOKEEPER=sid-hdf-g1-0.node.sid.consul:2181,sid-hdf-g1-1.node.sid.consul:2181,sid-hdf-g1-2.node.sid.consul:2181
+
+if [ $env = "SID" ]; then
+	NIMBUS=sid-hdf-g1-1.node.sid.consul
+	ZOOKEEPER=sid-hdf-g1-0.node.sid.consul:2181,sid-hdf-g1-1.node.sid.consul:2181,sid-hdf-g1-2.node.sid.consul:2181
+elif [ $env = "UAT" ]; then
+	NIMBUS=hdf-group2-1.node.consul
+	ZOOKEEPER=hdf-group2-0.node.consul:2181,hdf-group2-1.node.consul:2181,hdf-group4-2.node.consul:2181
+else
+	usage
+fi
+
 
 mvn clean package -P $PROFILE
 
